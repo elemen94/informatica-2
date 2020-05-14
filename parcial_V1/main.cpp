158e0ef233aa6c47d26bb5a5ecc2f75764790405
juan david londoño castaño <61588751+elemen94@users.noreply.github.com>
2020-05-14 15:44:53 -0500

    parcial_V1_combos

--- parcial_V1/main.cpp
+++ parcial_V1/main.cpp
@@ -2,8 +2,8 @@
 
 
 int main(){
-  int in=0,ad=0;
-  bool a;
+  int in=0,ad=0,us=0;
+  bool a,u;
   map<string, map<string, map<string, map<string,string>>>> mapa;
   map<char, map<string,int>> combo;
   map<char, list<char>> comparacion;
@@ -16,20 +16,21 @@ int main(){
       a = admin();
       volver:
       if(a == true){ system("cls"); ad = menu_admin(); }
-      else{ cout<<"Try again........ :("<<endl;system("pause");}
+      else{ cout<<"Try again........ :("<<endl;system("pause"); goto principio;}
       do{
         switch(ad){
           case 1:
+            leer_inventario("inventario.txt", mapa);
             leer_mapa(mapa);
             goto volver;
           case 2:
-             inventario(mapa);
+             agregar_inventario(mapa, "inventario.txt");
             goto volver;
           case 3:
-            crear_combo(combo, comparacion);
+            crear_combo(combo, comparacion, "combos.txt");
             goto volver;
           case 4:
-            ver_combos(combo);
+            ver_combos(combo, "combos.txt");
             goto volver;
           case 5:
             agregar_user("user.txt");
@@ -38,11 +39,27 @@ int main(){
             leer_user("user.txt");
             goto volver;
           case 7:
-            leer_inventario("inventario.txt", mapa);
             goto volver;
         }
       }while(ad<8 and ad>0);
         goto principio;
+    case 2:
+      u=user();
+      //volver_user:
+      if(u == true){ system("cls"); ver_combos(combo, "combos.txt"); us = menu_user(); }
+      else{ cout<<"Try again........ :("<<endl;system("pause");}
+      do{
+        switch(us){
+          case 1: break;
+        case 2: break;
+
+        }
+      }while(us<3 and us>0);
+      goto principio;
+
+
+
+
   }
   return 0;
 
