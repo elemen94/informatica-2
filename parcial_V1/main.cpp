babb1a785e39e5d152ff4fd6e6c4c1a5ec6f5003
juan david londoño castaño <61588751+elemen94@users.noreply.github.com>
2020-05-13 20:45:38 -0500

    parcial_V1_nuevo comienzo

--- parcial_V1/main.cpp
+++ parcial_V1/main.cpp
@@ -1,60 +1,42 @@
 #include "funciones.h"
 
 
-int main()
-{
-  int in,ad,us;
-  bool a;
-  map<char, map<char, int>> mapa;
-  ofstream esc;
-  ifstream lee;
+int main(){
+  int in=0,ad=0,us=0;
+  bool a,x;
+  map<char, map<int, map<string, map<int,int>>>> mapa;
+  principio:
   system("cls");
   in = menu();
-  switch (in) {
+  switch (in){
     case 1:
-      //system("cls");
       a = admin();
       volver:
       if(a == true){ system("cls"); ad = menu_admin(); }
-      else{ cout<<"Try again........ :("<<endl;
-        system("pause");}
+      else{ cout<<"Try again........ :("<<endl;system("pause");}
       do{
         switch(ad){
-       case 1:
-          leer_inventario("inventario_2.txt");
-          inventario(mapa,"inventario.txt");
-          //agregar_inventario(mapa);
-          goto volver;
-        case 2:
-          agregar_inventario(mapa,"inventario.txt","inventario_2.txt");
-          goto volver;
-          //break;
-        case 3:
-          leer_user("user.txt");
-          goto volver;
-        case 4:
-          agregar_user("user.txt");
-          goto volver;
-      }while(ad<6 and ad>0){
-        main();
-        ad=0;
-      break;
+          case 1:
+            leer_mapa(mapa);
+            goto volver;
+          case 2:
+             inventario(mapa);
+            //agregar_inventario(mapa,"inventario.txt","inventario_2.txt");
+            goto volver;
+          case 3:
+            leer_user("user.txt");
+            goto volver;
+          case 4:
+            //agregar_user("user.txt");
+            goto volver;
+          //case 5:
         }
-    case 2:
-      system("cls");
-      user();
-      menu_user();
-      do{
-        us = menu_user();
-      }while(us<3 and us>0);
-        main();
-        us=0;
-      break;
+      }while(ad<6 and ad>0);
+        goto principio;
   }
-while(in<3 and in>0);
-  system("cls");
-  if(in == 3) cout<<"Gracias te esperamos pronto...... ;)"<<endl;
-  else cout<<"Valor erroneo......"<<endl;
   return 0;
-}
-}
+
+ }
+
+
+
